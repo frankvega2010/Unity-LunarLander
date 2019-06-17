75894fede5ea7278f39a016efbffeadb6b186122
frankvega2010 <franco_vega@msn.com>
2019-06-17 00:04:33 -0300

    Added Lunar Module sprite & functionality

---
+++ First Contact/Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject particlesGameObject;
+    public float gas;
+
+    private PlayerMovement playerMovement;
+    private ParticleSystem playerParticles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        playerParticles = particlesGameObject.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerMovement.isMoving)
+        {
+            gas = gas - 0.1f;
+            if(gas <= 0)
+            {
+                playerMovement.isMoving = false;
+                gas = 0;
+            }
+            playerParticles.Play();
+        }
+        else
+        {
+            playerParticles.Stop();
+        }
+
+        if(gas <= 0)
+        {
+            playerMovement.canUseBoost = false;
+        }
+    }
+}
---
+++ First Contact/Assets/Scripts/Gameplay/Player/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float verticalForce;
+    public float horizontalForce;
+    public float gravity;
+    public bool isMoving;
+    public bool canUseBoost;
+
+    private Rigidbody2D playerRig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRig = GetComponent<Rigidbody2D>();
+        playerRig.gravityScale = gravity;
+        playerRig.AddForce(Vector3.right * 10, ForceMode2D.Impulse);
+        canUseBoost = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalSpeed = Input.GetAxisRaw("Horizontal");
+        float verticalSpeed = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(KeyCode.Space) || verticalSpeed > 0)
+        {
+            if(canUseBoost)
+            {
+                Debug.Log("is moving");
+                playerRig.AddForce(transform.up * verticalForce, ForceMode2D.Force);
+                isMoving = true;
+            }
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        if(horizontalSpeed < 0)
+        {
+            playerRig.AddTorque(horizontalForce);
+            CheckPlayerRotation(90, 265, 90);
+        }
+        else if (horizontalSpeed > 0)
+        {
+            playerRig.AddTorque(horizontalForce*-1);
+            CheckPlayerRotation(95, 270, 270);
+        }
+        else
+        {
+            playerRig.angularVelocity = 0;
+        }
+    }
+
+    private void CheckPlayerRotation(int angleRange1,int angleRange2,int newAngle)
+    {
+        float localRotZ = (transform.localEulerAngles.z + 360) % 360;
+        if (localRotZ > angleRange1 && localRotZ < angleRange2)
+        {
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, newAngle);
+            playerRig.angularVelocity = 0;
+        }
+    }
+}
