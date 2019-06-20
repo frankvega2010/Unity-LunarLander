f4b197d1a31941e71a9c6356fc5dd4319bc87c25
frankvega2010 <franco_vega@msn.com>
2019-06-20 09:53:31 -0300

    Added match time

---
+++ First Contact/Assets/Scripts/UI/UIDisplayTime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDisplayTime : MonoBehaviour
+{
+    public Text timeText;
+
+    private float timer;
+    private string time;
+    private int minutes;
+    private int seconds;
+    // Update is called once per frame
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        minutes = Mathf.FloorToInt(timer / 60F);
+        seconds = Mathf.FloorToInt(timer - minutes * 60);
+        time = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        timeText.text = "TIME  " + time;
+    }
+}
