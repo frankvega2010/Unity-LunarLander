b812fa104d651fe65aa162e0cb004139e5fbdcf8
frankvega2010 <franco_vega@msn.com>
2019-06-20 10:18:55 -0300

    Added player's altitude

--- First Contact/Assets/Scripts/UI/UIStatusManager.cs
+++ First Contact/Assets/Scripts/UI/UIStatusManager.cs
@@ -7,19 +7,23 @@ public class UIStatusManager : MonoBehaviour
     public GameObject player;
     public GameObject UIFuel;
     public GameObject UIScore;
+    public GameObject UIAltitude;
 
     private PlayerController playerStatus;
     private UIStatus fuel;
     private UIStatus score;
+    private UIStatus altitude;
     // Start is called before the first frame update
     void Start()
     {
         fuel = UIFuel.GetComponent<UIStatus>();
         score = UIScore.GetComponent<UIStatus>();
+        altitude = UIAltitude.GetComponent<UIStatus>();
         playerStatus = player.GetComponent<PlayerController>();
 
         fuel.statusName = "FUEL";
         score.statusName = "SCORE";
+        altitude.statusName = "ALTITUDE";
     }
 
     // Update is called once per frame
@@ -27,5 +31,6 @@ public class UIStatusManager : MonoBehaviour
     {
         fuel.statusValue = playerStatus.fuel;
         score.statusValue = playerStatus.score;
+        altitude.statusValue = player.transform.position.y*20;
     }
 }
