9a25392572c2c0240fbaa5f338ad7e14b1b4b43a
frankvega2010 <franco_vega@msn.com>
2019-06-20 11:04:55 -0300

    Added vertical Speed

--- First Contact/Assets/Scripts/UI/UIStatusManager.cs
+++ First Contact/Assets/Scripts/UI/UIStatusManager.cs
@@ -9,15 +9,18 @@ public class UIStatusManager : MonoBehaviour
     public GameObject UIScore;
     public GameObject UIAltitude;
     public GameObject UIHorizontalSpeed;
+    public GameObject UIVerticalSpeed;
 
     private PlayerController playerStatus;
     private UIStatus fuel;
     private UIStatus score;
     private UIStatus altitude;
     private UIStatus horizontalSpeed;
+    private UIStatus verticalSpeed;
 
     private Rigidbody2D playerRigidbody;
     private float hSpeed;
+    private float vSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,12 +28,14 @@ public class UIStatusManager : MonoBehaviour
         score = UIScore.GetComponent<UIStatus>();
         altitude = UIAltitude.GetComponent<UIStatus>();
         horizontalSpeed = UIHorizontalSpeed.GetComponent<UIStatus>();
+        verticalSpeed = UIVerticalSpeed.GetComponent<UIStatus>();
         playerStatus = player.GetComponent<PlayerController>();
 
         fuel.statusName = "FUEL";
         score.statusName = "SCORE";
         altitude.statusName = "ALTITUDE";
         horizontalSpeed.statusName = "HORIZONTAL SPEED";
+        verticalSpeed.statusName = "VERTICAL SPEED";
 
         playerRigidbody = player.GetComponent<Rigidbody2D>();
     }
@@ -39,9 +44,11 @@ public class UIStatusManager : MonoBehaviour
     void Update()
     {
         hSpeed = playerRigidbody.velocity.x;
+        vSpeed = playerRigidbody.velocity.y;
         fuel.statusValue = playerStatus.fuel;
         score.statusValue = playerStatus.score;
         altitude.statusValue = player.transform.position.y*20;
         horizontalSpeed.statusValue = Mathf.Abs(hSpeed * 40);
+        verticalSpeed.statusValue = Mathf.Abs(vSpeed * 40);
     }
 }
