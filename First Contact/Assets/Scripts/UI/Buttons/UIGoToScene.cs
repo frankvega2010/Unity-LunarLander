40b3215b9cbd70ce8ef54798fe4894e6096c2b4f
frankvega2010 <franco_vega@msn.com>
2019-06-21 12:15:32 -0300

    Added Pause Menu

---
+++ First Contact/Assets/Scripts/UI/Buttons/UIGoToScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIGoToScene : MonoBehaviour
+{
+    public string sceneName;
+    public bool isLevelRestartButton;
+
+    public void GoToThisScene()
+    {
+        switch (sceneName)
+        {
+            // WIP
+            default:
+                SceneManager.LoadScene(sceneName);
+                break;
+        }
+    }
+
+    public void QuitGame()
+    {
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+                Application.Quit();
+    #endif
+    }
+}
