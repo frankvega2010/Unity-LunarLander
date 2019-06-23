f47f7a5603ca54b27e888a8004c765ef548583f2
frankvega2010 <franco_vega@msn.com>
2019-06-23 10:05:26 -0300

    Added parallax effect on some screens

---
+++ First Contact/Assets/Scripts/Gameplay/Stage/BackgroundMoving.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMoving : MonoBehaviour
+{
+    public float speed;
+
+    private float timeBackground;
+    MeshRenderer backgroundRenderer;
+    private void Start()
+    {
+        backgroundRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeBackground += Time.deltaTime;
+        backgroundRenderer.material.mainTextureOffset = new Vector2(0, timeBackground * speed);
+    }
+}
